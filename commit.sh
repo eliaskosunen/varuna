e991b1890f62fac3d707886be975bc06d779af3c
Elias Kosunen <elias.kosunen@gmail.com>
2017-01-05 21:23:19 +0200

    Fix commit.sh

--- commit.sh
+++ commit.sh
@@ -2,8 +2,12 @@
 
 ./bin/tests
 
-git diff -U0 --no-color HEAD^ | clang-format-diff-3.9 -p1 -style=file -sort-includes
+#find src/ -iname "*.h" -o -iname "*.cpp" | xargs clang-format -i -style=file
+git diff -U0 --no-color HEAD^ | clang-format-diff-3.9 -p1 -style=file
 
 git status
+
+read -rsp $'Press any key to continue...\n' -n1 key
+
 git add .
 git commit
